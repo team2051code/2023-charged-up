c9f9d06ca76270bba81096335e4a1889ee405b54
Team 2051 <team2051@beattietech.com>
2023-02-25 11:18:47 -0500

    work in progress pneumatics

---
+++ src/main/java/frc/robot/subsystems/pneumatics.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class pneumatics extends SubsystemBase {
+  /** Creates a new pneumatics. */
+  public pneumatics() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+
+
+  Solenoid SolenoidPH = new Solenoid(PneumaticsModuleType.REVPH, 1);
+
+}
