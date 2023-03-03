b76a83cb34c7ecf0930af1bde95bee3c1ccf9192
Team 2051 <team2051@beattietech.com>
2023-03-02 21:46:25 -0500

    markt - wrist pivot sim; commented out safety

--- src/main/java/frc/robot/subsystems/simulated/ArmSimulation.java
+++ src/main/java/frc/robot/subsystems/simulated/ArmSimulation.java
@@ -24,12 +24,15 @@ public class ArmSimulation {
     public static final double ARM_MASS_KG = 8.0;
     public static final double ARM_BASE_LENGTH_M = Units.inchesToMeters(28);
     public static final double ARM_GEAR_REDUCTION = 2.0;
-    public static final double EXTENSION_MAX_SPEED_M_S = 0.5;
+    public static final double EXTENSION_MAX_SPEED_M_S = 0.25;
+    public static final double GRIPPER_MAX_SPEED_M_S = 90.0;
 
     private AnalogPotentiometerSimulation m_armPotentiometer;
     private CANSparkMax m_armMotor;
     private AnalogPotentiometerSimulation m_extensionPotentiometer;
     private CANSparkMax m_extensionMotor;
+    private AnalogPotentiometerSimulation m_gripperPotentiometer;
+    private CANSparkMax m_gripperMotor;
 
     private DCMotor m_armGearbox;
     private SingleJointedArmSim m_armSim;
@@ -38,15 +41,20 @@ public class ArmSimulation {
     private MechanismLigament2d m_armTowerDisplay;
     private MechanismLigament2d m_armExtensionDisplay;
     private MechanismLigament2d m_armDisplay;
+    private MechanismLigament2d m_gripperDisplay;
 
     public ArmSimulation(AnalogPotentiometerSimulation armPotentiometer,
     CANSparkMax armMotor,
     AnalogPotentiometerSimulation extensionPotentiometer,
-    CANSparkMax extensionMotor) {
+    CANSparkMax extensionMotor,
+    AnalogPotentiometerSimulation gripperPotentiometer,
+    CANSparkMax gripperMotor) {
         m_armPotentiometer = armPotentiometer;
         m_armMotor = armMotor;
         m_extensionPotentiometer = extensionPotentiometer;
         m_extensionMotor = extensionMotor;
+        m_gripperPotentiometer = gripperPotentiometer;
+        m_gripperMotor = gripperMotor;
 
         m_armGearbox = DCMotor.getFalcon500(2);
         m_armSim = new SingleJointedArmSim(
@@ -75,8 +83,12 @@ public class ArmSimulation {
             0,
             6,
             new Color8Bit(Color.kAqua)));
+        m_gripperDisplay = m_armExtensionDisplay.append(new MechanismLigament2d(
+            "Gripper", 
+            10, 0, 6, new Color8Bit(Color.kGreen)));
 
              m_extensionPotentiometer.set(0);
+             m_gripperPotentiometer.set(180);
 
         SmartDashboard.putData("Arm Sim", m_armWindowDisplay);
     }
@@ -86,7 +98,8 @@ public class ArmSimulation {
      */
     public void calculate() {
         SmartDashboard.putNumber("Simulated arm motor", m_armMotor.get());
-        // TODO: currently, arm sim ignores telescoping mass change and we need to sim the arm itself.
+        
+        // TODO: currently, arm sim ignores telescoping mass change.
         m_armSim.setInput(m_armMotor.get() * 12 /* volts */);
         m_armSim.update(0.02);
         SmartDashboard.putNumber("new arm angle", Units.radiansToDegrees(m_armSim.getAngleRads()));
@@ -102,6 +115,14 @@ public class ArmSimulation {
         m_extensionPotentiometer.set(currentArmExtension);
 
         m_armExtensionDisplay.setLength(currentArmExtension * 40);
+
+        var currentGripperAngle = m_gripperPotentiometer.get();
+
+        currentGripperAngle += m_gripperMotor.get() * GRIPPER_MAX_SPEED_M_S;
+        currentGripperAngle = MathUtil.clamp(currentGripperAngle, 180-55, 180+55);
+        m_gripperPotentiometer.set(currentGripperAngle);
+
+        m_gripperDisplay.setAngle(180 + currentGripperAngle);
 
 
     }
