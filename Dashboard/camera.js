860a52aa7940eb16080c5c1e039a3fda86af4bbc
Team 2051 <team2051code@gmail.com>
2023-03-06 21:11:22 -0500

    added logic for arm camera to flip with the arm -Joe

--- Dashboard/camera.js
+++ Dashboard/camera.js
@@ -1,6 +1,5 @@
 console.log("javaScrript has loaded");
 
-ctx = undefined;
 var isSaved = false;
 toggleCam = () => console.log("toggleCam");
 togglePi = () => console.log("togglePi");
@@ -11,20 +10,27 @@ function cameraInit(){
         const cameraPorts = [1183, 1181];
         let currentCamera = 0;
 
+        //22 is Shaolin Hand (testing)
+        //16 is Drill Handlespo Traffic Light and 20 is Dumb
         const piProtocols = [22, 20, 16];
         let currentPi = 0;
 
-        toggleCam = () => {
-            currentCamera = (currentCamera + 1) % cameraPorts.length;
-            const port = cameraPorts[currentCamera];
-            const piIp = piProtocols[currentPi];
+        frontCam = () => {
+            const piIp = piProtocols[1];
+            const port = cameraPorts[0];
             setCameraPort(piIp, port);
         }
 
-        togglePi = () => {
-            currentPi = (currentPi + 1) % piProtocols.length;
-            const piIp = piProtocols[currentPi];
-            const port = cameraPorts[currentCamera];
+        //if protocol/port need changed tweak url in ntLoaded
+        armCam = () => {
+            const piIp = piProtocols[2];
+            const port = cameraPorts[0];
+            setCameraPort(piIp, port);
+        }
+
+        backCam = () => {
+            const piIp = piProtocols[1];
+            const port = cameraPorts[1];
             setCameraPort(piIp, port);
         }
 
@@ -34,10 +40,22 @@ function cameraInit(){
         }
 
         setCameraPort(piProtocols[0], cameraPorts[0]);
-        
 }
 
+
 function ntLoaded() {
+
+    //filp camera output when the arm swiches sides AND arm camera is selected
+    NetworkTables.addKeyListener("/SmartDashboard/arm potentiometer", (key, value, isNew) => {
+        if(value > 180 && document.getElementById("cameraOut").src == "http://10.20.51.16:1183/stream.mjpg?1674930762001") {
+            document.getElementById("cameraOut").style.transform = "rotate(180deg)"
+        }
+        else {
+            document.getElementById("cameraOut").style.transform = "rotate(0deg)"
+        }
+    }, true);
+
+
     NetworkTables.addKeyListener("/SmartDashboard/leftStick", (key, value, isNew) => {
         console.log("network tables")
         var canvas = document.getElementById("canvas");
