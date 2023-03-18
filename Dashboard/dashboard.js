98ddba44f41f1e88cd730f1a59bd77e5d08a30ab
2051Code <2051Code@gmail.com>
2023-03-18 11:06:08 -0400

    added audio, time, and gear to dashboard -Joe

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -3,6 +3,10 @@ console.log("javaScrript has loaded");
 
 function ntLoaded() {
 
+    var isTeleop = false;
+    var soundPlayed = false;
+    var timeTrip = false;
+
     console.log("Network tables has loaded");
 
     NetworkTables.addGlobalListener((key, value, isNew) => {
@@ -17,7 +21,7 @@ function ntLoaded() {
         JSSoundTest.currentTime = 0;
         JSSoundTest.play()
     }, true);
-
+ 
     //background color of alliance
     NetworkTables.addKeyListener('/FMSInfo/IsRedAlliance', (key, value, isNew) => {
         if (key){
@@ -28,10 +32,35 @@ function ntLoaded() {
         }
     }, true)
 
-    //mode at top of dashboard
+    //mode and gear at top of dashboard
     NetworkTables.addKeyListener("/SmartDashboard/drop off mode", (key, value, isNew) => {
         if(value) document.getElementById("mode").innerText = "Drop Off";
         else document.getElementById("mode").innerText = "Pick Up"
     }, true);
+
+    NetworkTables.addKeyListener("/SmartDashboard/Gear", (key, value, isNew) => {
+        document.getElementById("gear").innerText = value ? "High" : "Low";
+    }, true)
+
+    //audio cues with time //
+    NetworkTables.addKeyListener("/SmartDashboard/Time", (key, value, isNew) => {
+        document.getElementById("time").innerText = value.toFixed(2)
+
+        value = Math.floor(value)
+        
+        if (value == -1 && !timeTrip){
+            console.log(isTeleop = !isTeleop);
+            timeTrip = true
+        }        
+
+        if (value % 5 == 0 && value <= 30 && !soundPlayed){
+                console.log('audio');
+                document.getElementById("soundTest").play();  
+                soundPlayed = true;          
+        }
+        else if (value % 5 == 4) {
+            soundPlayed = false;
+        }
+    }, true)
 
 }
